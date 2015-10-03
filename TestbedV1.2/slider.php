dceefcd1cc4a749036c2c85566aeea694fdf5e10
Ubuntu <ubuntu@ip-172-31-3-71.ap-southeast-2.compute.internal>
2015-10-03 01:27:10 +0000

    added function to allow user to set time interval between slides

--- TestbedV1.2/slider.php
+++ TestbedV1.2/slider.php
@@ -1,13 +1,20 @@
+<?php session_start();?>
 <?php require_once("php/header.php"); ?>
 <?php require_once("php/function.php"); ?>
-
+ <?php
+      if (isset($_SESSION['interval'])) {
+         $interval = $_SESSION['interval'];
+      }else {
+         $interval = '2000';
+      }
+      ?> 
 <?php
 
-
+//$interval = '5000';
 $slides = array('1','2', '3', '4', '5');
 ?>
 <div class="container">
-<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="2000">
+<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval = <?php echo  $interval; ?>  >
 <div class="carousel-inner" role="listbox">
      
 
